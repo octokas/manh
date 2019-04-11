6f1f28ed64ff56f1a2d89eff9627f0e5ee925159
stddef <batman@miskolczi>
2019-04-11 12:51:36 -0400

    introduction fleshed out

--- manh-ds/assets/global/js/custom.js
+++
@@ -1,5 +0,0 @@
-/* ============================================================
- * Written by Kaska Miskolczi for Manhattan & Associates
- =========================================================== */
-
-//****************** YOUR EXTRA EXTRA CUSTOMIZED JAVASCRIPT -- USER BEWARE **********************//
